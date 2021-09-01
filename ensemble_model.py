67307c68aa04fa89a0b304a9c2d16e7faf3fa66a
MasoumehVahedi <vahedi.m1993@gmail.com>
2021-09-01 12:31:56 +0430

    add evaluation model

--- ensemble_model.py
+++ ensemble_model.py
@@ -18,6 +18,7 @@ class ClassifierModel():
         self.parameters = parameters
 
         self.X_train, self.X_test, self.Y_train, self.Y_test = self.load_data()
+        self.model = self.train_model()
 
     def load_data(self):
         self.x = self.df.drop(["Churn", "customerID"], axis=1)
@@ -54,16 +55,16 @@ class ClassifierModel():
         # Predict the model
         predictions = model.predict(self.X_test)
 
-        # Evaluation model
-        cv = KFold(n_splits=self.num_fold, random_state=42, shuffle=True)
-        recall = cross_val_score(model, self.X, self.Y, cv=cv, scoring="recall", n_jobs=-1)
-        precision = cross_val_score(model, self.X, self.Y, cv=cv, scoring="precision", n_jobs=-1)
-        accuracy = cross_val_score(model, self.X, self.Y, cv=cv, scoring="accuracy", n_jobs=-1)
-        f1_score = cross_val_score(model, self.X, self.Y, cv=cv, scoring="f1_macro", n_jobs=-1)
-        print("Accuracy = {}".format(accuracy))
-        print("Accuracy in average = {}".format(np.mean(accuracy)))
-        print("Recall = {}".format(recall))
-        print("Precision = {}".format(precision))
-        print("f1_score = {}".format(f1_score))
+        return model
 
+    def evaluate_model(self):
+      cv = KFold(n_splits=self.num_fold, random_state=42, shuffle=True)
+      recall = cross_val_score(self.model, self.X, self.Y, cv=cv, scoring="recall", n_jobs=-1)
+      precision = cross_val_score(self.model, self.X, self.Y, cv=cv, scoring="precision", n_jobs=-1)
+      accuracy = cross_val_score(self.model, self.X, self.Y, cv=cv, scoring="accuracy", n_jobs=-1)
+      f1 = cross_val_score(self.model, self.X, self.Y, cv=cv, scoring="f1_macro", n_jobs=-1)
+      print("Accuracy in average = {}".format(np.mean(accuracy)))
 
+      # Display all metrics in a dataframe
+      metrics_df = pd.DataFrame([[accuracy, precision, recall, f1]], columns=["Accuracy", "Precision", "Recall", "F1 Score"])
+      return metrics_df
--- train.py
+++ train.py
@@ -14,7 +14,6 @@ from sklearn.tree import DecisionTreeClassifier
 
 
 
-
 if __name__ == "__main__":
     data = pd.read_csv(config.dataset_dir)
     # Encode Categorical Columns
@@ -26,32 +25,56 @@ if __name__ == "__main__":
     xgb_clf = XGBClassifier
     xgb_model = ClassifierModel(df=data, classifier=xgb_clf, num_fold=config.NUM_FOLD, parameters=config.xgb_params)
     xgb_model.train_model()
+    # Evaluation model
+    xgb_metrics = xgb_model.evaluate_model()
+    xgb_metrics.index = ["XGBoost"]
+    results = xgb_metrics
 
     # GBoost Classifier
     gb_clf = GradientBoostingClassifier
     gb_model = ClassifierModel(df=data, classifier=gb_clf, num_fold=config.NUM_FOLD, parameters=config.gb_params)
     gb_model.train_model()
+    # Evaluation model
+    gb_metrics = gb_model.evaluate_model()
+    gb_metrics.index = ["GBoost"]
+    results = results.append(gb_metrics)
 
     # AdaBoost Classifier
     ada_clf = AdaBoostClassifier
     ada_model = ClassifierModel(df=data, classifier=ada_clf, num_fold=config.NUM_FOLD, parameters=config.ada_params)
     ada_model.train_model()
+    # Evaluation model
+    ada_metrics = ada_model.evaluate_model()
+    ada_metrics.index = ["AdaBoost"]
+    results = results.append(ada_metrics)
 
     # Random Forest Classifier
     rf_clf = RandomForestClassifier
     rf_model = ClassifierModel(df=data, classifier=rf_clf, num_fold=config.NUM_FOLD, parameters=config.rf_params)
     rf_model.train_model()
+    # Evaluation model
+    rf_metrics = rf_model.evaluate_model()
+    rf_metrics.index = ["Random Forest"]
+    results = results.append(rf_metrics)
 
     # Decision Tree Classifier
     dt_clf = DecisionTreeClassifier
     dt_model = ClassifierModel(df=data, classifier=dt_clf, num_fold=config.NUM_FOLD, parameters=config.dt_params)
     dt_model.train_model()
+    # Evaluation model
+    dt_metrics = dt_model.evaluate_model()
+    dt_metrics.index = ["Decision Tree"]
+    results = results.append(dt_metrics)
 
     # ExtraTreesClassifier
     et_clf = ExtraTreesClassifier
     et_model = ClassifierModel(df=data, classifier=et_clf, num_fold=config.NUM_FOLD, parameters=config.et_params)
     et_model.train_model()
-
+    # Evaluation model
+    et_metrics = et_model.evaluate_model()
+    et_metrics.index = ["ExtraTrees"]
+    results = results.append(et_metrics)
+    print(results.head())
 
 
 
